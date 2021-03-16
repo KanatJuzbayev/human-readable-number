c58ea40f1fac10657863ee73d9abd8604c24bbdd
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-03-16 21:42:55 +0600

    feat: add function changes

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ module.exports = function toReadable (number) {
   if (number < 21) {
     var Num = num[number];
   } else if (number > 20 || number < 100) {
-        if (num[parseInt(strNum[1])] == num[0]) {
+        if (parseInt(strNum[1]) == 0) {
             Num = dozens[parseInt(strNum[0])];
         } else {
             Num = dozens[parseInt(strNum[0])] + ' ' + num[parseInt(strNum[1])];
@@ -49,18 +49,16 @@ module.exports = function toReadable (number) {
     }
 
   if (number >= 100) {
-      if (num[parseInt(strNum[2])] == num[0]) {
-        Num = num[parseInt(strNum[0])] + ' hundred ' + dozens[parseInt(strNum[1])]; 
-      } else if (dozens[parseInt(strNum[1])] == dozens[0]) {
-        Num = num[parseInt(strNum[0])] + ' hundred ' + num[number % 100];
-      } else if (parseInt(strNum[2]) === 0 & parseInt(strNum[1]) === 0) {
-        Num = num[parseInt(strNum[0])] + ' hundred';
-      } else if (parseInt(strNum[2]) !== 0 & parseInt(strNum[1]) !== 0) {
-          Num = num[parseInt(strNum[0])] + ' hundred ' + dozens[parseInt(strNum[1])] + ' ' + num[parseInt(strNum[2])];
-      }
-      
-  }
-    
+      if (parseInt(strNum[2]) == 0 & parseInt(strNum[1]) !== 1) {
+        Num = num[parseInt(strNum[0])] + ' hundred ' + dozens[parseInt(strNum[1])];
+        if (parseInt(strNum[1]) == 0) {
+          Num = num[parseInt(strNum[0])] + ' hundred';
+        }
+      } else if (parseInt(strNum[1]) < 2) {
+        Num = num[parseInt(strNum[0])] + ' hundred '  + num[parseInt(strNum[1] + strNum[2])];
+      } else {
+        Num = num[parseInt(strNum[0])] + ' hundred ' + dozens[parseInt(strNum[1])] + ' ' + num[parseInt(strNum[2])];
+      }            
+  }    
   return Num;
 }
-
